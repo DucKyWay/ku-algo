66c72277a5194122f31c83a50c1f53df38e91926
DucKyWay <niji2549@gmail.com>
2025-12-07 20:05:51 +0700

    catch up

--- w01/cascading.cc
+++ w01/cascading.cc
@@ -3,7 +3,31 @@
 #include <vector>
 using namespace std;
 
+string plus(int a, int b) {
+
+}
+
+string minus(int a, int b) {
+
+}
+
+string multiply(int a, int b) {
+
+}
+
+string divide(int a, int b) {
+
+}
+
+
 int main() {
+
+    vector<string> val;
+    
+    string in;
+    while(cin>>in && in != EOF) {
+      val.add(in);
+    }
 
     return 0;
 }

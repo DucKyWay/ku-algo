166eaa54e43a41890fd6c3494d0b291729f593f8
DucKyWay <niji2549@gmail.com>
2025-12-11 09:59:25 +0700

    update

--- w01/cascading.cpp
+++ w01/cascading.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
 using namespace std;
 
-void multiply(string &a, string &b) {
-    cout << a << " * " << b << endl;
+string multiply(string &a, string &b) {
+    cout << a << " " << b << endl;
+
+    if(a == "0" || b == "0") return "0";
+
+    int n = a.size(), m = b.size();
+    vector<int> result(n + m, 0);
+    cout << "n: " << n << endl << "m: " << m << endl;
+
+    for(int i = n - 1; i >= 0; i--) {
+
+        int ia = a[i] - '0';
+        for(int j = m - 1; j >= 0; j--) {
+
+        }
+    }
+
+    string res = "";
+    bool lead = true;
+    // ขยับ 0
+    for(int i = 0; i < (int)result.size(); i++) {
+        if(lead && result[i] == 0) {
+            continue;
+        }
+        lead = false;
+        res.push_back(char(result[i] = '0'));
+    }
+
+    if(res == "") return "0";
+    return res;
 }
 
 int main() {
@@ -21,7 +49,7 @@ int main() {
 
     string res = "1";
     for (int i = 0; i < (int)val.size(); i++) {
-        multiply(res, val[i]);
+        res = multiply(res, val[i]);
     }
 
     cout << res << endl;

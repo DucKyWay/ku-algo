12118952c6fee3e1e70124468212e180c9240fb1
DucKyWay <niji2549@gmail.com>
2025-12-26 19:15:48 +0700

    finish cascading

--- 01/cascading.cpp
+++ 01/cascading.cpp
@@ -3,36 +3,41 @@
 #include <string>
 using namespace std;
 
-string multiply(string &a, string &b) {
-    cout << a << " " << b << endl;
-
-    if(a == "0" || b == "0") return "0";
+string multiply(const string &a, const string &b) {
+    if (a == "0" || b == "0") return "0";
 
     int n = a.size(), m = b.size();
     vector<int> result(n + m, 0);
-    cout << "n: " << n << endl << "m: " << m << endl;
 
-    for(int i = n - 1; i >= 0; i--) {
+    for (int i = n - 1; i >= 0; --i) {
+        int da = a[i] - '0';
+        for (int j = m - 1; j >= 0; --j) {
+            int db = b[j] - '0';
 
-        int ia = a[i] - '0';
-        for(int j = m - 1; j >= 0; j--) {
-            cout << ia << j << endl;
+            int posLow = i + j + 1;
+            int posHigh = i + j;
+
+            int sum = result[posLow] + da * db; // + ทด
+            result[posLow] = sum % 10; // หน่วย
+            result[posHigh] += sum / 10; // ทดไป
         }
     }
 
-    string res = "";
-    bool lead = true;
     // ขยับ 0
-    for(int i = 0; i < (int)result.size(); i++) {
-        if(lead && result[i] == 0) {
-            continue;
-        }
-        lead = false;
-        res.push_back(char(result[i] = '0'));
+    string res;
+    int k = 0;
+    while (k < (int)result.size() && result[k] == 0) {
+        k++;
+    }
+    for (; k < (int)result.size(); ++k) {
+        res.push_back(char('0' + result[k]));
     }
 
-    if(res == "") return "0";
-    return res;
+    if (res.empty()) {
+        return "0";
+    } else {
+        return res;
+    }
 }
 
 int main() {
@@ -42,16 +47,13 @@ int main() {
     while (cin >> in) {
         val.push_back(in);
     }
-
-    if (val.empty()) {
-        return 0;
-    }
+    if (val.empty()) return 0;
 
     string res = "1";
     for (int i = 0; i < (int)val.size(); i++) {
         res = multiply(res, val[i]);
     }
 
-    cout << res << endl;
+    cout << res << "\n";
     return 0;
 }
